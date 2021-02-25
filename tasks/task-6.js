37ca640bde274bcaa01d8d07c7b989a83e7ee68a
paul <mail@paulgrotzke.com>
2021-02-25 18:57:06 +0100

    current state

--- tasks/task-6.js
+++ tasks/task-6.js
@@ -1,27 +1,26 @@
-
 const users = {
   'id-1': {
     name: 'user-1',
     address: {
       street: 'street 1',
-      city: 'city 1'
+      city: 'city 1',
     },
     friends: {
       'id-2': {
         name: 'user-2',
         address: {
           street: 'street 2',
-          city: 'city 1'
+          city: 'city 2',
         },
-        friends: ['id-1', 'id-2']
-      }
-    }
+        friends: ['id-1', 'id-2'],
+      },
+    },
   },
   'id-2': {
     name: 'user-2',
     address: {
       street: 'street 2',
-      city: 'city 1'
+      city: 'city 2',
     },
     friends: {
       'id-1': {
@@ -29,41 +28,30 @@ const users = {
         name: 'user-1',
         address: {
           street: 'street 1',
-          city: 'city 1'
+          city: 'city 1',
         },
-        friends: ['id-2']
+        friends: ['id-2'],
       },
       'id-3': {
         id: 'id-3',
         name: 'user-3',
         address: {
           street: 'street 3',
-          city: 'city 3'
+          city: 'city 3',
         },
-        friends: []
-      }
-    }
+        friends: [],
+      },
+    },
   },
   'id-3': {
     name: 'user-3',
     address: {
       street: 'street 3',
-      city: 'city 2'
+      city: 'city 3',
     },
-    friends: {}
+    friends: {},
   },
-}
-
-
-
-// flatt to array
-function format (users) {
-  // to implement
-}
-
-const result = format(users)
-
-console.log(JSON.stringify(result, null, 2))
+};
 
 const expectedResult = [
   {
@@ -71,25 +59,60 @@ const expectedResult = [
     name: 'user-1',
     address: {
       street: 'street 1',
-      city: 'city 1'
+      city: 'city 1',
     },
-    friends: ['id-2']
-  },{
+    friends: ['id-2'],
+  },
+  {
     id: 'id-2',
     name: 'user-2',
     address: {
       street: 'street 2',
-      city: 'city 2'
+      city: 'city 2',
     },
-    friends: ['id-1', 'id-3']
-  },{
+    friends: ['id-1', 'id-3'],
+  },
+  {
     id: 'id-3',
     name: 'user-3',
     address: {
       street: 'street 3',
-      city: 'city 3'
+      city: 'city 3',
     },
-    friends: []
+    friends: [],
   },
-]
+];
 
+const result = format(users);
+
+// flatt to array
+function format(users) {
+  let formattedFriends = {};
+  for (let key in users) {
+    formattedFriends = {
+      ...formattedFriends,
+      [key]: Object.keys(users[key]['friends']),
+    };
+  }
+
+  let result = [];
+  for (let key in users) {
+    result.push({
+      id: key,
+      name: users[key]['name'],
+      address: {
+        street: users[key]['address']['street'],
+        city: users[key]['address']['city'],
+      },
+      friends: formattedFriends[key],
+    });
+  }
+
+  return result;
+}
+
+console.log(JSON.stringify(result, null, 2));
+console.log(
+  JSON.stringify(result, null, 2) ===
+    JSON.stringify(expectedResult, null, 2),
+);
